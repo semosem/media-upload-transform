4e58e47e6fc96ee170a375e092167dc75ee0a5de
Sem G <akukulu@AI-Pro.local>
2026-02-02 15:48:55 +0200

    chore: add cloudinary-nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,49 @@
+"use client";
 import Image from "next/image";
+import { CldVideoPlayer, getCldImageUrl } from "next-cloudinary";
+import "next-cloudinary/dist/cld-video-player.css";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+        <CldVideoPlayer
+          width="1920"
+          height="1080"
+          // poster={getCldImageUrl({
+          //   tint: "equalize:80:blue:blueviolet",
+
+          //   src: "",
+          // })}
+          src="https://res.cloudinary.com/videocrop/video/upload/female_ej5j44.mp4"
+          colors={{
+            accent: "#ff0000",
+            base: "#00ff00",
+            text: "#0000ff",
+          }}
+          transformation={{
+            width: 500,
+            height: 500,
+            crop: "fill",
+            opacity: 0.8,
+            overlays: [
+              {
+                publicId:
+                  "https://res.cloudinary.com/videocrop/image/upload/c_pad,b_auto:predominant_gradient,w_400,ar_1/v1769975359/king_qafgin.jpeg",
+                position: {
+                  x: 50,
+                  y: 50,
+                  gravity: "north_west",
+                },
+                appliedEffects: [
+                  {
+                    multiply: true,
+                  },
+                ],
+              },
+            ],
+          }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
       </main>
     </div>
   );
